1611e5df270e51d7ad284285448903a32002d186
neursh <31346263+neursh@users.noreply.github.com>
2025-11-04 16:55:15 +0700

    Boilerplates

---
+++ build.sh
@@ -0,0 +1,15 @@
+ehco Building QRGen WASM...
+cd qrgen
+cargo install wasm-pack
+wasm-pack build --target web
+cd ..
+
+echo Building server...
+cd server
+cargo build --release
+cd ..
+
+echo Building web...
+cd web
+yarn build
+cd ..
---
+++ qrgen/README.md
@@ -0,0 +1,4 @@
+# QRGen WASM
+A WASM package to generate QR codes!
+
+This is the backbone for Kewar to generate QR code on the client side.
