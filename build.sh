0a3797cf016f167697f871de7375e1d7a3b23b48
neursh <31346263+neursh@users.noreply.github.com>
2025-11-05 08:16:56 +0700

    Simple build file

--- build.sh
+++ build.sh
@@ -1,15 +1,61 @@
-echo Building QRGen WASM...
-cd qrgen
-cargo install wasm-pack
-wasm-pack build --target web
-cd ..
+function build_qrgen() {
+  echo \[BUILD\] Building qrgen...
+  cd qrgen
+  cargo install wasm-pack
+  if ! wasm-pack build --target web --release; then
+    echo "Build qrgen failed."
+    exit 1
+  fi
 
-echo Building server...
-cd server
-cargo build --release
-cd ..
+  echo \[BUILD\] Editing generated build...
+  sed -i "s|        module_or_path = new URL('qrgen_wasm_bg.wasm', import.meta.url);|        module_or_path = new URL('/qrgen/qrgen_wasm_bg.wasm', import.meta.url);|g" pkg/qrgen_wasm.js
+  edit_status=$?
 
-echo Building web...
-cd web
-yarn build
-cd ..
+  if [ $edit_status -eq 1 ]; then
+    echo "Configure qrgen failed."
+    exit 1
+  fi
+
+  echo \[BUILD\] Moving wasm build to web\'s /public directory...
+  mkdir -p ../web/public/qrgen && mv pkg/qrgen_wasm_bg.wasm ../web/public/qrgen
+  move_status=$?
+
+  if [ $move_status -eq 1 ]; then
+    echo "Configure qrgen failed."
+    exit 1
+  fi
+}
+
+function build_server() {
+  echo \[BUILD\] Building server...
+  cd ../server
+  if ! cargo build --release; then
+    echo "Build server failed."
+    exit 1
+  fi
+}
+
+function build_web() {
+  echo \[BUILD\] Building web...
+  cd ../web
+  if ! yarn & yarn build; then
+    echo "Build web failed."
+    exit 1
+  fi
+}
+
+if [ $1 == "qrgen" ]; then
+  build_qrgen
+fi
+if [ $1 == "server" ]; then
+  build_server
+fi
+if [ $1 == "web" ]; then
+  build_web
+fi
+if [ $1 == "all" ]; then
+  build_qrgen
+  build_server
+  build_web
+fi
+
