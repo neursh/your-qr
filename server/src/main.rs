2e1892d5ad38ebbe1bb0267eb9af9551646aaa0f
neursh <31346263+neursh@users.noreply.github.com>
2025-11-06 20:42:57 +0700

    Stich everything together

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use axum::Router;
+
+use crate::services::{ WorkerSpecs, construct_services };
+
+mod routes;
+mod services;
+
+#[tokio::main]
+async fn main() {
+  let args: Vec<String> = env::args().collect();
+  let host = &args[1];
+
+  let services_channel = construct_services(WorkerSpecs {
+    hash_pass: (4, 2048),
+  });
+
+  let app = Router::new().nest("/users", routes::users::routes(services_channel));
+
+  let listener = tokio::net::TcpListener::bind(host).await.unwrap();
+  axum::serve(listener, app).await.unwrap();
 }
