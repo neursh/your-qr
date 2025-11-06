1d3002559b4377f12c1226a38d64ea80bb3b5e7e
neursh <31346263+neursh@users.noreply.github.com>
2025-11-06 20:43:15 +0700

    I like this more

--- qrgen/src/lib.rs
+++ qrgen/src/lib.rs
@@ -1,4 +1,4 @@
-use qrcode::{ Color, EcLevel, QrCode };
+use qrcode::{ EcLevel, QrCode };
 use wasm_bindgen::prelude::*;
 
 fn primitive_convert<D: AsRef<[u8]>>(data: D, correction_level: u8) -> Result<QrCode, String> {
@@ -39,33 +39,6 @@ fn convert_to_vec<D: AsRef<[u8]>>(data: D, correction_level: u8) -> Result<Vec<u
   return Ok(bool_map);
 }
 
-fn convert_to_svg<D: AsRef<[u8]>>(data: D, correction_level: u8) -> Result<String, String> {
-  let code = primitive_convert(data, correction_level)?;
-  let module_count = code.width();
-  let color_map = code.into_colors();
-
-  let mut path_data = String::new();
-
-  for y in 0..module_count {
-    let mut x = 0;
-    while x < module_count {
-      let idx = y * module_count + x;
-      if color_map[idx] == Color::Dark {
-        let mut width = 1;
-        while x + width < module_count && color_map[y * module_count + x + width] == Color::Dark {
-          width += 1;
-        }
-        path_data.push_str(&format!("M{} {}h{}v{}H{}z", x, y, width, 1, x));
-        x += width;
-      } else {
-        x += 1;
-      }
-    }
-  }
-
-  return Ok(path_data);
-}
-
 #[wasm_bindgen]
 #[allow(non_snake_case)]
 pub fn fromStringToQr(string: &str, correction_level: u8) -> Result<Vec<u8>, String> {
@@ -80,12 +53,27 @@ pub fn fromBytesToQr(data: Vec<u8>, correction_level: u8) -> Result<Vec<u8>, Str
 
 #[wasm_bindgen]
 #[allow(non_snake_case)]
-pub fn fromStringtoQrSvg(string: &str, correction_level: u8) -> Result<String, String> {
-  return convert_to_svg(string, correction_level);
-}
+pub fn fromQrToSvgPath(qr: Vec<u8>) -> String {
+  let module_count = (qr.len() as f64).sqrt() as usize;
 
-#[wasm_bindgen]
-#[allow(non_snake_case)]
-pub fn fromBytesToQrSvg(data: Vec<u8>, correction_level: u8) -> Result<String, String> {
-  return convert_to_svg(data, correction_level);
+  let mut path_data = String::new();
+
+  for y in 0..module_count {
+    let mut x = 0;
+    while x < module_count {
+      let idx = y * module_count + x;
+      if qr[idx] == 1 {
+        let mut width = 1;
+        while x + width < module_count && qr[y * module_count + x + width] == 1 {
+          width += 1;
+        }
+        path_data.push_str(&format!("M{} {}h{}v{}H{}z", x, y, width, 1, x));
+        x += width;
+      } else {
+        x += 1;
+      }
+    }
+  }
+
+  return path_data;
 }
