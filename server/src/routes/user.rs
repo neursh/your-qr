07cd51a2bd5dfe992f5d2995bf0cdd77acdff909
neursh <31346263+neursh@users.noreply.github.com>
2025-11-09 13:36:38 +0700

    Add limiter for creating user endpoint

--- server/src/routes/user.rs
+++ server/src/routes/user.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
+
 use axum::{ Router, routing::post };
+use tokio::sync::Semaphore;
 
 use crate::{ database::KewarCollections, services::Services };
 
@@ -7,15 +10,22 @@ pub mod delete;
 pub mod structs;
 
 #[derive(Clone)]
+pub struct RoutesLimiter {
+  pub create: Arc<Semaphore>,
+}
+
+#[derive(Clone)]
 pub struct RouteState {
   pub services: Services,
   pub kewar_collections: KewarCollections,
+  pub limiters: RoutesLimiter,
 }
 
 pub fn routes(services: Services, kewar_collections: KewarCollections) -> Router {
   let state = RouteState {
     services,
     kewar_collections,
+    limiters: RoutesLimiter { create: Arc::new(Semaphore::new(8)) },
   };
   Router::new().route("/", post(create::handle).delete(delete::handle)).with_state(state)
 }
--- server/src/routes/user/create.rs
+++ server/src/routes/user/create.rs
@@ -1,8 +1,9 @@
 use axum::{ Json, extract::State, http::StatusCode };
+use mongodb::bson::doc;
 
 use crate::{
   base::{ self, response::ResponseModel },
-  database::users::UserStore,
+  database::users::UserDocument,
   routes::user::{ RouteState, structs::CreateUserPayload },
 };
 use nanoid::nanoid;
@@ -11,17 +12,32 @@ pub async fn handle(
   State(state): State<RouteState>,
   Json(payload): Json<CreateUserPayload>
 ) -> (StatusCode, Json<ResponseModel>) {
+  if state.limiters.create.acquire().await.is_err() {
+    return base::response::internal_error();
+  }
+
+  match state.kewar_collections.users.get_one(doc! { "email": payload.email.clone() }).await {
+    Ok(user) => {
+      if user.is_some() {
+        return base::response::error(
+          StatusCode::BAD_REQUEST,
+          "An account with the same email already exists."
+        );
+      }
+    }
+    Err(error) => {
+      return parse_db_fail(error);
+    }
+  }
+
   let password_hash = match state.services.hash_pass.send(payload.password).await {
     Ok(Some(hash)) => hash,
     _ => {
-      return base::response::error(
-        StatusCode::INTERNAL_SERVER_ERROR,
-        "Something went wrong while processing your request."
-      );
+      return base::response::internal_error();
     }
   };
 
-  let user = UserStore {
+  let user = UserDocument {
     _id: nanoid!(),
     email: payload.email,
     password_hash,
@@ -45,10 +61,7 @@ fn parse_db_fail(error: mongodb::error::Error) -> (StatusCode, Json<ResponseMode
     }
     _ => {
       eprintln!("Database error: {:?}", error.kind);
-      return base::response::error(
-        StatusCode::INTERNAL_SERVER_ERROR,
-        "Something went wrong while processing your request."
-      );
+      return base::response::internal_error();
     }
   }
 }
